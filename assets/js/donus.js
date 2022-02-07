756cb4128e4a3d53bcd6c610496c80da72e2054e
yanis.pirlet <yanis.pirlet@gmail.com>
2022-02-07 13:18:34 +0100

    stats orga merge

--- assets/js/donus.js
+++ assets/js/donus.js
@@ -1,3 +1,111 @@
+//prendre le dernier caractère du lien
+var mdr_id = window.location.href;
+mdr_id = mdr_id.slice(-6)
+mdr_id = mdr_id.charAt(0);
+mdr_id -= 1
+
+const mdr1 = [{
+  competence: 'accueil',
+  val: 12,
+}, {
+  competence: 'activité',
+  val: 7,
+}, {
+  competence: 'relaction',
+  val: 7,
+}, {
+  competence: 'entretien',
+  val: 7,
+}, {
+  competence: 'rapidité',
+  val: 6,
+}, {
+  competence: 'modernité',
+  val: 5,
+}];
+
+const mdr2=[{
+  competence: 'accueil',
+  val: 17,
+}, {
+  competence: 'activité',
+  val: 5,
+}, {
+  competence: 'relaction',
+  val: 7,
+}, {
+  competence: 'entretien',
+  val: 9,
+}, {
+  competence: 'rapidité',
+  val: 6,
+}, {
+  competence: 'modernité',
+  val: 3,
+}]
+
+const mdr3=[{
+  competence: 'accueil',
+  val: 13,
+}, {
+  competence: 'activité',
+  val: 8,
+}, {
+  competence: 'relaction',
+  val: 15,
+}, {
+  competence: 'entretien',
+  val: 7,
+}, {
+  competence: 'rapidité',
+  val: 6,
+}, {
+  competence: 'modernité',
+  val: 5,
+}]
+const mdr4=[{
+  competence: 'accueil',
+  val: 11,
+}, {
+  competence: 'activité',
+  val: 7,
+}, {
+  competence: 'relaction',
+  val: 7,
+}, {
+  competence: 'entretien',
+  val: 7,
+}, {
+  competence: 'rapidité',
+  val: 6,
+}, {
+  competence: 'modernité',
+  val: 18,
+}]
+const mdr6=[{
+  competence: 'accueil',
+  val: 13,
+}, {
+  competence: 'activité',
+  val: 7,
+}, {
+  competence: 'relaction',
+  val: 7,
+}, {
+  competence: 'entretien',
+  val: 7,
+}, {
+  competence: 'rapidité',
+  val: 6,
+}, {
+  competence: 'modernité',
+  val: 5,
+}]
+
+list_mdr = [mdr1, mdr2, mdr3, mdr4, mdr6]
+var dataSource = list_mdr[mdr_id]
+console.log(mdr_id)
+console.log(dataSource)
 $(() => {
   $('#pie').dxPieChart({
     size: {
@@ -42,24 +150,3 @@ $(() => {
     }
   }
 });
-
-
-const dataSource = [{
-  competence: 'accueil',
-  val: 12,
-}, {
-  competence: 'activité',
-  val: 7,
-}, {
-  competence: 'relaction',
-  val: 7,
-}, {
-  competence: 'entretien',
-  val: 7,
-}, {
-  competence: 'rapidité',
-  val: 6,
-}, {
-  competence: 'modernité',
-  val: 5,
-}];
