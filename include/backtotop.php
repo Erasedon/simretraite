cf37cc46b500c64cd078497df4f3133cc165b06f
Isooka <Andyh08440@gmail.com>
2022-02-07 13:46:42 +0100

    amélioration php

---
+++ include/backtotop.php
@@ -0,0 +1,2 @@
+<div class="grannybbw">
+    <a id="back-top" href="#top"><i class="fa fa-chevron-up"></i></a></div>
---
+++ include/footer.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+            <ul class="social-icon">
+              <li class="social-icon__item"><a class="social-icon__link" href="#">
+                  <ion-icon name="logo-facebook"></ion-icon>
+                </a></li>
+              <li class="social-icon__item"><a class="social-icon__link" href="#">
+                  <ion-icon name="logo-twitter"></ion-icon>
+                </a></li>
+              <li class="social-icon__item"><a class="social-icon__link" href="#">
+                  <ion-icon name="logo-linkedin"></ion-icon>
+                </a></li>
+              <li class="social-icon__item"><a class="social-icon__link" href="#">
+                  <ion-icon name="logo-instagram"></ion-icon>
+                </a></li>
+            </ul>
+            <ul class="menu">
+              <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
+              <li class="menu__item"><a class="menu__link" href="#">Téléphone</a></li>
+              <li class="menu__item"><a class="menu__link" href="#">Mail</a></li>
+        
+            </ul>
+            <p>&copy;2022 Nina Yanis Andy | Tout droit réserver</p>
+        </footer>
